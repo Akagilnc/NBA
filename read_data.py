05c6f4754f5435b988ac688166ad16e796dc75b5
DeronLee <akagilnc@gmail.com>
2017-08-09 23:36:44 +0800

    made NBA.xlsx

--- read_data.py
+++ read_data.py
@@ -8,12 +8,21 @@ with open('./datas/warriors.json') as data_file:
     data_value = datas.get('value')
 
     wb = load_workbook('NBA.xlsx')
-    sheet = wb.get_active_sheet
-
+    sheet = wb.create_sheet()
+    sheet.title = data_title
+    row_num = 1
 
     for data in data_value:
-        print(data)
+        row_num += 1
+        sheet.cell(column=2, row=row_num, value=data['x'])
+        sheet.cell(column=3, row=row_num, value=data['y'])
+        if 'p' in data.keys():
+            sheet.cell(column=5, row=row_num, value=data['p'])
+        if 'p' in data.keys() and data['p'] != 'f':
+            sheet.cell(column=4, row=row_num, value=data['t'])
+            sheet.cell(column=6, row=row_num, value=data['o'])
+            sheet.cell(column=7, row=row_num, value=data['d'])
 
-
+    wb.save('NBA.xlsx')
     print(data_title)
 
